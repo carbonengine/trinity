e5e120933f0963a7fb8e576f0ecabc7fba080772
Anonymous CCP Employee <anonymous.employee@ccpgames.com>
2014-05-20 10:24:27 +0000

    Porting using //eve/development/MAINLINE<-//eve/staging/2014-DOLLY at head (786028)
    Binaries submitted in 786182
    Build time : 17 minutes 42.64000 seconds
    Branch : //eve/branches/development/MAINLINE/
    
    [git-p4: depot-paths = "//eve/branches/development/MAINLINE/carbon/src/Trinity/": change = 786073]

--- Tr2AtlasTexture.cpp
+++ Tr2AtlasTexture.cpp
@@ -166,7 +166,11 @@ BlueAsyncRes::LoadingResult Tr2AtlasTexture::DoLoad()
 	if( result )
 	{
 		m_loadedBitmap->ConvertFormat( PIXEL_FORMAT_B8G8R8A8_UNORM );
-		m_textureAtlas = g_textureAtlasMan->FindAtlas( m_loadedBitmap->GetFormat() );
+		// Allow loading to a specific atlas by setting this before DoLoad
+		if( !m_textureAtlas )
+		{
+			m_textureAtlas = g_textureAtlasMan->FindAtlas( m_loadedBitmap->GetFormat() );
+		}
 	}
 	else
 	{
