b6991338b4bdada9c7fa28966a05f5e32d634665
filipppavlov <filipppavlov@users.noreply.github.com>
2016-05-18 09:43:46 +0000

    Fixed TrinityALTests
    Binaries submitted in 1041364
    Build time : 0 minutes 52.822000 seconds
    Branch : //eve/branches/development/MAINLINE/
    
    [git-p4: depot-paths = "//eve/branches/development/MAINLINE/carbon/src/": change = 1041358]

--- tests/Shaders.DX11/compile.cmd
+++ tests/Shaders.DX11/compile.cmd
@@ -5,8 +5,8 @@ for %%i in (*.psh) do (
     ..\fxc\fxc /nologo /Tps_5_0 /DPATCHED_SHADER=1 /Fo %%~ni.ps.patched.bin %%i
 )
 for %%i in (*.vsh) do (
-    \depot\games\shared_tools\FXCompiler\feb2010\fxc /nologo /Tvs_5_0 /Fo %%~ni.vs.bin %%i
-    \depot\games\shared_tools\FXCompiler\feb2010\fxc /nologo /Tvs_5_0 /DPATCHED_SHADER=1 /Fo %%~ni.vs.patched.bin %%i
+    ..\fxc\fxc /nologo /Tvs_5_0 /Fo %%~ni.vs.bin %%i
+    ..\fxc\fxc /nologo /Tvs_5_0 /DPATCHED_SHADER=1 /Fo %%~ni.vs.patched.bin %%i
 )
 for %%i in (*.bin) do (
     call ..\bin2h.cmd %%~ni.bin %%~ni.h
--- tests/bin2h.cmd
+++ tests/bin2h.cmd
@@ -8,19 +8,27 @@ rem ^
 import argparse
 
 
+def _hex(d, d1, d2, d3):
+    return '0x{:0>2x}{:0>2x}{:0>2x}{:0>2x}'.format(d3, d2, d1, d)
+
+
 def _to_hex(s):
-    l = []
+    l = ''
     for i in xrange(0, len(s), 4):
         d3 = ord(s[i + 3]) if i + 3 < len(s) else 0
         d2 = ord(s[i + 2]) if i + 2 < len(s) else 0
         d1 = ord(s[i + 1]) if i + 1 < len(s) else 0
-        d = (d3 << 24) | (d2 << 16) | (d1 << 8) | (ord(s[i]) << 0)
-        l.append(hex(d))
-    return ', '.join(l)
+        d = ord(s[i])
+        if l:
+            l += ', '
+        if i and i % 16 == 0:
+            l += '\n'
+        l += _hex(d, d1, d2, d3)
+    return l
 
 
 def convert_file(input, output):
-    with open(input) as f:
+    with open(input, 'rb') as f:
         contents = f.read()
     contents = _to_hex(contents)
     with open(output, 'w') as f:
