7c693ce62a8ddec68b8a45e443ece5035a861ce3
Anonymous CCP Employee <anonymous.employee@ccpgames.com>
2018-07-19 15:17:44 +0000

    Test cases for GPU timer. Done at same time as https://jira.ccpgames.com/browse/GFX-4416
    [git-p4: depot-paths = "//eve/branches/development/MAINLINE/carbon/src/": change = 1353220]

---
+++ tests/GPUTimer.cpp
@@ -0,0 +1,46 @@
+#include "StdAfx.h"
+#include "WithValidRenderContextFixture.h"
+#include "WithRenderContextFixture.h"
+#include <Windows.h>
+
+using namespace Tr2RenderContextEnum;
+
+TEST_F( WithValidRenderContext, TimerIsInvalidBeforeCreation )
+{
+	Tr2GpuTimerAL timer;
+	EXPECT_FALSE(timer.IsValid());
+}
+
+TEST_F( WithRenderContext, CreatingWithoutRenderContext )
+{
+	Tr2GpuTimerAL timer;
+	auto hrResult = timer.Create(*renderContext);
+	ASSERT_HRESULT_FAILED(hrResult);
+}
+
+TEST_F( WithValidRenderContext, IsValidAfterCreation )
+{
+	Tr2GpuTimerAL timer;
+	auto hrResult = timer.Create(*renderContext);
+	ASSERT_HRESULT_SUCCEEDED(hrResult);
+	EXPECT_TRUE(timer.IsValid());
+}
+
+TEST_F( WithValidRenderContext, Begins )
+{
+	Tr2GpuTimerAL timer;
+	const auto hrResult = timer.Create(*renderContext);
+	ASSERT_HRESULT_SUCCEEDED(hrResult);
+	const auto begins = timer.Begin(*renderContext);
+	EXPECT_TRUE(begins);
+}
+
+TEST_F( WithValidRenderContext, ValidAfterStopping )
+{
+	Tr2GpuTimerAL timer;
+	const auto hrResult = timer.Create(*renderContext);
+	ASSERT_HRESULT_SUCCEEDED(hrResult);
+	timer.Begin(*renderContext);
+	timer.End(*renderContext);
+	EXPECT_TRUE(timer.IsValid());
+}
