f966e07c0a79fcf3f2da7a8d158897ef417d0b1b
filipppavlov <filipppavlov@users.noreply.github.com>
2019-09-23 15:51:11 +0000

    Trying to fix compile errors on clang
    [git-p4: depot-paths = "//eve/branches/development/MAINLINE/carbon/src/": change = 1572939]

--- gles2/Tr2TextureALGLES2.cpp
+++ gles2/Tr2TextureALGLES2.cpp
@@ -1066,11 +1066,11 @@ namespace TrinityALImpl
 			size += m_desc.GetMipSize( i );
 		}
 
-		description["size"] = std::to_string( long long( size ) );
-		description["width"] = std::to_string( long long( m_desc.GetWidth() ) );
-		description["height"] = std::to_string( long long( m_desc.GetHeight() ) );
-		description["mipLevels"] = std::to_string( long long( m_desc.GetTrueMipCount() ) );
-		description["format"] = std::to_string( long long( m_desc.GetFormat() ) );
+		description["size"] = std::to_string( (long long)( size ) );
+		description["width"] = std::to_string( (long long)( m_desc.GetWidth() ) );
+		description["height"] = std::to_string( (long long)( m_desc.GetHeight() ) );
+		description["mipLevels"] = std::to_string( (long long)( m_desc.GetTrueMipCount() ) );
+		description["format"] = std::to_string( (long long)( m_desc.GetFormat() ) );
 	}
 }
 
