487f6a14776aee4713485683501a31a987e86610
ccpnobody <55136070+ccpnobody@users.noreply.github.com>
2024-09-30 15:31:41 +0000

    PostProcessVolumes
    Adding a postprocess volume child object for manipulation and application of postprocesses
    
    Renamed EveChildEnvironment to EvechildPostProcessVolume
    
    https://ccpgames.atlassian.net/browse/PLAT-5348

--- trinity/PostProcess/Tr2PostProcess2.h
+++ trinity/PostProcess/Tr2PostProcess2.h
@@ -33,6 +33,7 @@ BLUE_DECLARE( Tr2PPFilmGrainEffect );
 BLUE_DECLARE( Tr2PPDesaturateEffect );
 BLUE_DECLARE( Tr2PPFadeEffect );
 BLUE_DECLARE( Tr2PPLutEffect );
+BLUE_DECLARE_VECTOR( Tr2PPLutEffect );
 BLUE_DECLARE( Tr2PPVignetteEffect );
 BLUE_DECLARE( Tr2PPFogEffect );
 BLUE_DECLARE( Tr2PPTaaEffect );
@@ -49,23 +50,33 @@ public:
 	Tr2PostProcess2( IRoot* lockobj = NULL );
 	~Tr2PostProcess2();
 
-	Tr2PPSignalLossEffectPtr GetSignalLoss() { return m_signalLoss; }
-	Tr2PPGodRaysEffectPtr GetGodRays() { return m_godRays; }
-	Tr2PPBloomEffectPtr GetBloom() { return m_bloom; }
-	Tr2PPDynamicExposureEffectPtr GetDynamicExposure() { return m_dynamicExposure; }
-	Tr2PPFilmGrainEffectPtr GetFilmGrain() { return m_filmGrain; }
-	Tr2PPDesaturateEffectPtr GetDesaturate() { return m_desaturate; }
-	Tr2PPFadeEffectPtr GetFade() { return m_fade; }
-	Tr2PPLutEffectPtr GetLut() { return m_lut; }
-	Tr2PPLutEffectPtr GetAdditionalLut1() { return m_additionalLut1; }
-	Tr2PPLutEffectPtr GetAdditionalLut2() { return m_additionalLut2; }
-	Tr2PPLutEffectPtr GetAdditionalLut3() { return m_additionalLut3; }
-	Tr2PPVignetteEffectPtr GetVignette() { return m_vignette; }
-	Tr2PPFogEffectPtr GetFog() { return m_fog; }
-	Tr2PPTaaEffectPtr GetTaa() { return m_taa; }
-	Tr2PPDepthOfFieldEffectPtr GetDepthOfField() { return m_depthOfField; }
-	Tr2PPTonemappingEffectPtr GetTonemapping() { return m_tonemapping; }
+	Tr2PPSignalLossEffectPtr GetSignalLoss() const { return m_signalLoss; }
+	Tr2PPGodRaysEffectPtr GetGodRays() const{ return m_godRays; }
+	Tr2PPBloomEffectPtr GetBloom() const { return m_bloom; }
+	Tr2PPDynamicExposureEffectPtr GetDynamicExposure() const { return m_dynamicExposure; }
+	Tr2PPFilmGrainEffectPtr GetFilmGrain() const { return m_filmGrain; }
+	Tr2PPDesaturateEffectPtr GetDesaturate() const { return m_desaturate; }
+	Tr2PPFadeEffectPtr GetFade() const { return m_fade; }
+	void GetLuts( std::vector<const Tr2PPLutEffect*> &container ) const;
+	Tr2PPVignetteEffectPtr GetVignette() const { return m_vignette; }
+	Tr2PPFogEffectPtr GetFog() const { return m_fog; }
+	Tr2PPTaaEffectPtr GetTaa() const { return m_taa; }
+	Tr2PPDepthOfFieldEffectPtr GetDepthOfField() const { return m_depthOfField; }
+	Tr2PPTonemappingEffectPtr GetTonemapping() const { return m_tonemapping; }
 
+	void SetSignalLoss(Tr2PPSignalLossEffectPtr effect) { m_signalLoss = effect; }
+	void SetGodRays(Tr2PPGodRaysEffectPtr effect) { m_godRays = effect; }
+	void SetBloom(Tr2PPBloomEffectPtr effect) { m_bloom = effect; }
+	void SetDynamicExposure(Tr2PPDynamicExposureEffectPtr effect) { m_dynamicExposure = effect; }
+	void SetFilmGrain(Tr2PPFilmGrainEffectPtr effect) { m_filmGrain = effect; }
+	void SetDesaturate(Tr2PPDesaturateEffectPtr effect) { m_desaturate = effect; }
+	void SetFade(Tr2PPFadeEffectPtr effect) { m_fade = effect; }
+	void AddLut(Tr2PPLutEffectPtr effect) { m_luts.Append(effect); }
+	void SetVignette(Tr2PPVignetteEffectPtr effect) { m_vignette = effect; }
+	void SetFog(Tr2PPFogEffectPtr effect) { m_fog = effect; }
+	void SetTaa(Tr2PPTaaEffectPtr effect) { m_taa = effect; }
+	void SetDepthOfField(Tr2PPDepthOfFieldEffectPtr effect) { m_depthOfField = effect; }
+	void SetTonemapping(Tr2PPTonemappingEffectPtr effect) { m_tonemapping = effect; }
 
 	// Helper method for scenes to decide on miplodbias
 	float GetMipLodBias() const;
@@ -82,10 +93,7 @@ private:
 	Tr2PPFilmGrainEffectPtr m_filmGrain;
 	Tr2PPDesaturateEffectPtr m_desaturate;
 	Tr2PPFadeEffectPtr m_fade;
-	Tr2PPLutEffectPtr m_lut;
-	Tr2PPLutEffectPtr m_additionalLut1;
-	Tr2PPLutEffectPtr m_additionalLut2;
-	Tr2PPLutEffectPtr m_additionalLut3;
+	PTr2PPLutEffectVector m_luts;
 	Tr2PPVignetteEffectPtr m_vignette;
 	Tr2PPFogEffectPtr m_fog;
 	Tr2PPTaaEffectPtr m_taa;
