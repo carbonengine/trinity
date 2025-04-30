14df43f0d68162aeab5da69fc856b85d49f5ccb5
ccp-intern <ccp-intern@users.noreply.github.com>
2025-04-30 15:08:42 +0200

    alpha cutout shadows feature flag

--- trinity/Eve/SpaceObjectFactory/EveSOF.cpp
+++ trinity/Eve/SpaceObjectFactory/EveSOF.cpp
@@ -57,6 +57,10 @@
 #include "Utilities/BoundingSphere.h"
 #include "BlueObjectMetadata.h"
 #include "ITr2TextureProvider.h"
+#include "TriSettingsRegistrar.h"
+
+bool g_alphaCutoutShadowsEnabled = false; // alpha cutout refers to TRIBATCHTYPE_DECAL
+TRI_REGISTER_SETTING( "alphaCutoutShadowsEnabled", g_alphaCutoutShadowsEnabled );
 
 namespace
 {
@@ -555,8 +559,7 @@ size_t EveSOF::FillMeshAreaVector( Tr2MeshAreaVector* meshAreaVector, TriBatchTy
 		{
 		case TRIBATCHTYPE_DECAL:
 			newShader->SetOption( BlueSharedString( "SPACE_OBJECT_TRANSPARENCY" ), BlueSharedString( "SOT_CLIP" ) );
-			// TODO: intern, add feature flag for frontier only
-			castsShadows = true;
+			castsShadows = g_alphaCutoutShadowsEnabled;
 			break;
 		case TRIBATCHTYPE_TRANSPARENT:
 			newShader->SetOption( BlueSharedString( "SPACE_OBJECT_TRANSPARENCY" ), BlueSharedString( "SOT_TRANSPARENT" ) );
@@ -2104,7 +2107,15 @@ void EveSOF::SetupShaders( const EveSOFDNAPtr dna, Tr2MeshBase* mesh ) const
 	{
 		size_t cntr = 0;
 		cntr += FillMeshAreaVector( mesh->GetAreas( TRIBATCHTYPE_OPAQUE ), TRIBATCHTYPE_OPAQUE, dna, hullIdx, meshIndexOffset );
-		cntr += FillMeshAreaVector( mesh->GetAreas( TRIBATCHTYPE_DECAL ), TRIBATCHTYPE_DECAL, dna, hullIdx, meshIndexOffset );
+		// if we don't use alpha cutout shadows, then putting the decals into the opaque bucket might save some performance
+		if ( g_alphaCutoutShadowsEnabled )
+		{
+			cntr += FillMeshAreaVector( mesh->GetAreas( TRIBATCHTYPE_DECAL ), TRIBATCHTYPE_DECAL, dna, hullIdx, meshIndexOffset );
+		}
+		else
+		{
+			cntr += FillMeshAreaVector( mesh->GetAreas( TRIBATCHTYPE_OPAQUE ), TRIBATCHTYPE_DECAL, dna, hullIdx, meshIndexOffset );
+		}
 		cntr += FillMeshAreaVector( mesh->GetAreas( TRIBATCHTYPE_TRANSPARENT ), TRIBATCHTYPE_TRANSPARENT, dna, hullIdx, meshIndexOffset );
 		cntr += FillMeshAreaVector( mesh->GetAreas( TRIBATCHTYPE_ADDITIVE ), TRIBATCHTYPE_ADDITIVE, dna, hullIdx, meshIndexOffset );
 		cntr += FillMeshAreaVector( mesh->GetAreas( TRIBATCHTYPE_DISTORTION ), TRIBATCHTYPE_DISTORTION, dna, hullIdx, meshIndexOffset );
