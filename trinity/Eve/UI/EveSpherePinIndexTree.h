bcee5f4c13577e1be7336fd7f5652844c35a2833
filipppavlov <filipppavlov@users.noreply.github.com>
2025-06-12 14:30:25 +0000

    Changed how we extract mesh information when building an acceleration structure for sphere decals: instead of reading VRAM back, load TriGrannyRes and use its data.

--- trinity/Eve/UI/EveSpherePinIndexTree.h
+++ trinity/Eve/UI/EveSpherePinIndexTree.h
@@ -2,7 +2,8 @@
 #ifndef EveSpherePinIndexTree_H
 #define EveSpherePinIndexTree_H
 
-struct TriGeometryResMeshData;
+BLUE_DECLARE( TriGrannyRes );
+
 
 class EveSpherePinIndexTree
 {
@@ -10,14 +11,16 @@ public:
 	struct Face;
 	struct TreeNode;
 
-	EveSpherePinIndexTree(void);
+	explicit EveSpherePinIndexTree( TriGrannyRes* granny );
 	~EveSpherePinIndexTree(void);
 
-	int Initialize( TriGeometryResMeshData* mesh );
+	int Initialize();
 	int GetIndices( Vector3& point, float radius, int& primitives, std::vector<unsigned short>& indices );
 
 	int IsInitialized() { return m_initialized; }
 private:
+	TriGrannyResPtr m_granny;
+
 	// A spherically partitioned binary tree of all the faces in the source geometry.
 	TreeNode* m_tree;
 
