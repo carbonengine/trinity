daff5621874f6008e1235fa66f9a208a21efa624
ccp-intern <ccp-intern@users.noreply.github.com>
2025-07-17 18:34:29 +0200

    reintroduce old tonemapper as option

--- trinity/PostProcess/Effects/Tr2PPTonemappingEffect.h
+++ trinity/PostProcess/Effects/Tr2PPTonemappingEffect.h
@@ -17,14 +17,34 @@ public:
 	Tr2PPTonemappingEffect( IRoot* lockobj = NULL );
 	~Tr2PPTonemappingEffect();
 
-	float m_slope;
-	float m_toe;
-	float m_shoulder;
-	float m_blackClip;
-	float m_whiteClip;
-	float m_scale;
-	float m_blueCorrection;
-	bool m_useSweeteners;
+	struct
+	{
+		float m_shoulderStrength;
+		float m_linearStrength;
+		float m_linearAngle;
+		float m_toeStrength;
+		float m_toeNumerator;
+		float m_toeDenominator;
+		float m_whiteScale;
+	} m_uncharted2;
+
+	struct
+	{
+		float m_slope;
+		float m_toe;
+		float m_shoulder;
+		float m_blackClip;
+		float m_whiteClip;
+		float m_scale;
+		float m_blueCorrection;
+		bool m_useSweeteners;
+	} m_aces;
+
+	enum
+	{
+		Uncharted2,
+		Aces
+	} m_method;
 };
 
 TYPEDEF_BLUECLASS( Tr2PPTonemappingEffect );
