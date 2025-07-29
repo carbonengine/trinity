afecf4b3a53b76e5e43c4ab188561b543e1c5b0d
ccp-intern <ccp-intern@users.noreply.github.com>
2025-07-29 17:23:35 +0200

    Update Tr2PPBloomEffect.cpp

--- trinity/PostProcess/Effects/Tr2PPBloomEffect.cpp
+++ trinity/PostProcess/Effects/Tr2PPBloomEffect.cpp
@@ -6,13 +6,9 @@
 
 #include "StdAfx.h"
 #include "Tr2PPBloomEffect.h"
-#include "TriSettingsRegistrar.h"
-
-float g_defaultBloomLuminanceThreshold = -1.0f;
-TRI_REGISTER_SETTING( "defaultBloomLuminanceThreshold", g_defaultBloomLuminanceThreshold );
 
 Tr2PPBloomEffect::Tr2PPBloomEffect( IRoot* lockobj ) :
-	m_luminanceThreshold( g_defaultBloomLuminanceThreshold ),
+	m_luminanceThreshold( -1.0f ),
 	m_luminanceScale( 0.5f ),
 	m_bloomBrightness( 0.2f ),
 	m_sizeScale( 4.0f ),
