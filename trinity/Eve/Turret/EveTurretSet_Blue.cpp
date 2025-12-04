e4f01bc157caaaf3b6defa6da2993066d47783e1
ccpisheden <136614133+ccpisheden@users.noreply.github.com>
2025-12-04 12:48:48 +0100

    Added comments

--- trinity/Eve/Turret/EveTurretSet_Blue.cpp
+++ trinity/Eve/Turret/EveTurretSet_Blue.cpp
@@ -119,7 +119,7 @@ const Be::ClassInfo* EveTurretSet::ExposeToBlue()
 		MAP_ATTRIBUTE( "sysBonePitch03Factor", m_sysBonePitch03Factor, "pitch 03 factor", Be::READWRITE | Be::PERSIST )
 		MAP_ATTRIBUTE( "sysBonePitch03Offset", m_sysBonePitch03Offset, "pitch 03 offset (in degrees!)", Be::READWRITE | Be::PERSIST )
 
-		MAP_ATTRIBUTE( "firingEffect", m_firingEffect, "", Be::HIDDEN )
+		MAP_ATTRIBUTE( "firingEffect", m_firingEffect, "", Be::HIDDEN ) // Needed to make Graphite able to detect bindings inside of the firing effect.
 		MAP_PROPERTY( "firingEffect", GetFiringEffect, SetFiringEffect, "The module for the firing effect of this turret" )
 		MAP_ATTRIBUTE( "firingEffectResPath", m_firingEffectResPath, "A res path to the redfile containing the primary firing effect", Be::READWRITE | Be::PERSIST )
 
@@ -137,7 +137,7 @@ const Be::ClassInfo* EveTurretSet::ExposeToBlue()
 		MAP_ATTRIBUTE( "chooseRandomLocator", m_chooseRandomLocator, "Allow choosing a random locator instead of the closest one", Be::READWRITE | Be::PERSIST )
 		MAP_ATTRIBUTE( "randomizeExplosionRotation", m_randomizeExplosionRotation, "Can be unchecked to have the explosion use the damage-locator rotation instead", Be::READWRITE | Be::PERSIST )
 
-		MAP_ATTRIBUTE( "ambientEffect", m_ambientEffect, "", Be::PERSISTONLY )
+		MAP_ATTRIBUTE( "ambientEffect", m_ambientEffect, "", Be::PERSISTONLY ) // Needed to make Graphite able to detect bindings inside of the ambient effect.
 		MAP_PROPERTY( "ambientEffect", GetAmbientEffect, SetAmbientEffect, "The ambient effect around the turret" )
 		MAP_ATTRIBUTE( "generatedDistributedAmbientEffect", m_generatedDistributedAmbientEffect, "The generated and distributed ambient effect across all turret locations", Be::READ )
 		MAP_ATTRIBUTE( "ambientEffectEditingMode", m_ambientEffectEditingMode, "This toggles on/off the ambient effect editing mode \n:jessica-group: Editing", Be::READWRITE | Be::NOTIFY )
